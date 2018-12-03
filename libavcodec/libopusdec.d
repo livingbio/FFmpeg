174b0c608e6cd798192bd60b88e25cddfc5570ad
Stephen Chen <stephenchen@gliacloud.com>
2018-12-04 01:52:08 +0800

    ready-to-install

---
+++ libavcodec/libopusdec.d
@@ -0,0 +1,22 @@
+libavcodec/libopusdec.o: libavcodec/libopusdec.c /usr/include/opus/opus.h \
+ /usr/include/opus/opus_types.h /usr/include/opus/opus_defines.h \
+ /usr/include/opus/opus_multistream.h /usr/include/opus/opus.h \
+ libavutil/internal.h config.h libavutil/attributes.h libavutil/timer.h \
+ libavutil/log.h libavutil/avutil.h libavutil/common.h libavutil/macros.h \
+ libavutil/version.h libavutil/avconfig.h libavutil/intmath.h \
+ libavutil/x86/intmath.h libavutil/mem.h libavutil/error.h \
+ libavutil/internal.h libavutil/rational.h libavutil/mathematics.h \
+ libavutil/intfloat.h libavutil/pixfmt.h libavutil/x86/timer.h \
+ libavutil/cpu.h libavutil/dict.h libavutil/x86/emms.h \
+ libavutil/attributes.h libavutil/cpu.h libavutil/libm.h \
+ libavutil/intreadwrite.h libavutil/bswap.h libavutil/x86/bswap.h \
+ libavutil/x86/intreadwrite.h libavutil/ffmath.h libavutil/opt.h \
+ libavutil/samplefmt.h libavcodec/avcodec.h libavutil/samplefmt.h \
+ libavutil/avutil.h libavutil/buffer.h libavutil/channel_layout.h \
+ libavutil/dict.h libavutil/frame.h libavutil/buffer.h \
+ libavutil/hwcontext.h libavutil/frame.h libavutil/log.h \
+ libavutil/pixfmt.h libavutil/rational.h libavcodec/version.h \
+ libavutil/version.h libavcodec/internal.h libavutil/mathematics.h \
+ libavcodec/vorbis.h libavcodec/mathops.h libavutil/common.h \
+ libavutil/reverse.h libavcodec/x86/mathops.h libavutil/x86/asm.h \
+ libavcodec/libopus.h
